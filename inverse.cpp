8c4486d922cf12d54f3e6e5b9850d26bd265ca74
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-07-31 23:17:56 +0530

    Add files via upload

---
+++ inverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,l;
+    cout<<"Enter a number to inverse";
+    cin>>n;
+    while(n>0)
+      {l=n%10;
+      cout<<""<<l;
+      n=n/10;
+      
+      }
+
+}
+//Enter a number to inverse12345
+//54321
+
+//=== Code Execution Successful ===
